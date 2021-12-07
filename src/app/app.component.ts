aed0290040a649aa115d6654c7ec4759e7557622
Antonio DroCode <antuansabe@gmail.com>
2021-12-07 13:40:48 -0600

    add control structures

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,26 @@ export class AppComponent {
   name = 'Antonn';
   age = 18;
   btnDisabled = true;
+  person = {
+    name: 'Antonn',
+    age: 18,
+  }
+
+  toggleButton() {
+      this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge(){
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event){
+    const element = event.target as HTMLElement;
+
+  }
 }
