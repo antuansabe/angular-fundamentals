3a8c718e4b4cc03893d9b84d5a5366099e51f408
Antonio DroCode <antuansabe@gmail.com>
2021-12-07 17:03:36 -0600

    add ngfor to render images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Product } from  './product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,6 +16,42 @@ export class AppComponent {
 
 
   names: string[ ]= [ 'Lola' , 'Ronnie', 'Filly','Santi', 'Bobi'];
+  newName = '';
+  products: Product[] = [
+    {
+      name: 'El mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
+
+
   toggleButton() {
       this.btnDisabled = !this.btnDisabled;
   }
@@ -32,5 +68,13 @@ export class AppComponent {
   changeName(event: Event){
     const element = event.target as HTMLElement;
 
+  }
+
+  addName() {
+    this.names.push(this.newName);
+  }
+
+  deleteName(index: number ) {
+    this.names.splice(index,1);
   }
 }
