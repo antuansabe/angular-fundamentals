bd1b053b6bcd3b9f64d3be9f83a7b9ff130c6cb1
Antonio DroCode <antuansabe@gmail.com>
2021-12-07 20:27:50 -0600

    add ngstyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Product } from  './product.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  widthImg = 10;
   name = 'Antonn';
   age = 18;
   btnDisabled = true;
